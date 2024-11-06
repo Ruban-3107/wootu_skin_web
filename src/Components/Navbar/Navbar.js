c1c2c133db797a4b87d092d872b4ce45c0bc7ff6
uiuxvignesh <vignesh.venkatesan@ehealthforce.com>
2024-11-06 21:30:54 +0530

    development

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,116 +1,183 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, NavDropdown, Container, Button } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+"use client"; // This makes the Navbar component a Client Component
+import { useEffect, useState } from "react"; // Only keep this correct import
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter } from "react-router-dom";
+import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
+import About from "../../pages/About";
+import Contact from "../../pages/Contact";
+import Home from "../../pages/Home";
+import TreatmentPage from "../../pages/TreatmentPage";
+import css from "./Navbar.css";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../Assets/Wootlogo.png";
-import './Navbar.css'; // Ensure this path is correct
 
-const skinTreatments = [
-  'Hydra Facial', 'Chemical Peel', 'Carbon Laser', 'Photo Facial', 'Glycolic Peel', 'Nano Peel', 'Light Peel',
-  'Combination Peel', 'Yellow Peel', 'Salicylic Peel', 'Glutathione Peel'
-];
-
-const hairTreatments = [
-  'Upper Lip', 'Chin', 'Upper Arms', 'Full Hands', 'Full Legs', 'Bikini', 'Full Body', 'Side Locks', 
-  '3 Body Part LHR'
+const skin = [
+  "Hydra Facial",
+  "Chemical Peel",
+  "Carbon Laser",
+  "Photo Facial",
+  "Glycolic Peel",
+  "Nano Peel",
+  "Light Peel",
+  "Combination Peel",
+  "Yellow Peel",
+  "Salicylic Peel",
+  "Glutathione Peel",
+  "Upper Lip",
+  "Chin",
+  "Upper Arms",
+  "Full hands",
+  "Full legs",
+  "Bikini",
+  "Full Body",
+  "Side Locks",
+  "3 Body part LHR",
 ];
 
 const slimming = [
-  'Lymphatic Drainage', 'Abdomen Tucks', 'Back Tucks', 'Thigh Tucks', 'Arm Tucks', 'Hip Tucks', 
-  'Green Coffee-Abdomen, Arms, Hip', 'Neck', 'Lipoflush', 'ULS', 'NMS', 'Cello', 'Cryo (Cool Sculpting)', 
-  'Diet Plans per Month'
+  "Lymphatic Drainage",
+  "Abdomen Tucks",
+  "Back Tucks",
+  "Thigh Tucks",
+  "Arm Tucks",
+  "Hip Tucks",
+  "Green Coffee-Abdomen, Arms, Hip",
+  "Neck",
+  "Lipoflush",
+  "ULS",
+  "NMS",
+  "Cello",
+  "Cryo(Cool Sculpting)",
+  "Diet plans per month",
 ];
 
 const CustomNavbar = () => {
+  const [showMegaMenu, setShowMegaMenu] = useState(false);
+  const [activeMenu, setActiveMenu] = useState("");
   const navigate = useNavigate();
 
-  const handleNavigate = (label) => {
-    navigate('/TreatmentPage', { state: { label } });
+  const handleMenuHover = (menuType) => {
+    setShowMegaMenu(true);
+    setActiveMenu(menuType);
   };
 
+  const handleNavigate = (label) => {
+    console.log(label + "treatmentPage");
+    navigate("/TreatmentPage", { state: { label } });
+  };
   return (
-    <Navbar expand="lg" className="navbar-custom">
+    <Navbar expand="lg" className="py-0">
       <Container>
-        {/* Logo */}
+        {/* Left side - Logo */}
         <Navbar.Brand href="#">
-          <img src={logo} alt="Logo" width="100" height="100" />
+          <img
+            src={logo} // Replace with your logo path
+            alt="Logo"
+            width="100"
+            height="100"
+          />
         </Navbar.Brand>
 
-        {/* Toggle Button */}
+        {/* Centered Navbar items */}
         <Navbar.Toggle aria-controls="navbarResponsive" />
+        <Navbar.Collapse
+          id="navbarResponsive"
+          className="justify-content-center"
+          style={{height:'110px'}}
+        >
+          <Nav className="nav-items h-100">
+            {/* Home */}
+            <Nav.Link href="/Home">Home</Nav.Link>
+            {/* Sliming with dropdown */}
+            <Nav.Link
+              onMouseEnter={() => handleMenuHover("slimming")}
+              onMouseLeave={() => setShowMegaMenu(false)}
+            >
+              Slimming
+            </Nav.Link>
 
-        {/* Collapsible Navbar */}
-        <Navbar.Collapse id="navbarResponsive">
-          <Nav className="mx-auto nav-items">
-            <Nav.Link href="/Home" className="nav-item-custom">Home</Nav.Link>
+            <Nav.Link
+              onMouseEnter={() => handleMenuHover("skin")}
+              onMouseLeave={() => setShowMegaMenu(false)}
+            >
+              Skin & Hare
+            </Nav.Link>
+            {/* Career */}
+            <Nav.Link to="/">Career</Nav.Link>
 
-            {/* Slimming Dropdown */}
-            <NavDropdown title="Slimming" id="slimming-dropdown" className="nav-item-custom">
-              {slimming.map((treatment) => (
-                <NavDropdown.Item 
-                  key={treatment} 
-                  onClick={() => handleNavigate(treatment)}
-                  className="dropdown-item-custom"
-                >
-                  {treatment}
-                </NavDropdown.Item>
-              ))}
-            </NavDropdown>
+            {/* About */}
+            <Nav.Link href="/About">About</Nav.Link>
 
-            {/* Skin Dropdown */}
-            <NavDropdown title="Skin" id="skin-dropdown" className="nav-item-custom">
-              {skinTreatments.map((treatment) => (
-                <NavDropdown.Item 
-                  key={treatment} 
-                  onClick={() => handleNavigate(treatment)}
-                  className="dropdown-item-custom"
-                >
-                  {treatment}
-                </NavDropdown.Item>
-              ))}
-            </NavDropdown>
-
-            {/* Hair Dropdown */}
-            <NavDropdown title="Hair" id="hair-dropdown" className="nav-item-custom">
-              {hairTreatments.map((treatment) => (
-                <NavDropdown.Item 
-                  key={treatment} 
-                  onClick={() => handleNavigate(treatment)}
-                  className="dropdown-item-custom"
-                >
-                  {treatment}
-                </NavDropdown.Item>
-              ))}
-            </NavDropdown>
-
-            <Nav.Link href="/" className="nav-item-custom">Career</Nav.Link>
-            <Nav.Link href="/About" className="nav-item-custom">About</Nav.Link>
-            <Nav.Link href="/Contact" className="nav-item-custom">Contact</Nav.Link>
+            {/* Contact */}
+            <Nav.Link href="/Contact">Contact</Nav.Link>
           </Nav>
-
-          {/* Buttons for Mobile */}
-          <div className="d-lg-none d-flex flex-column mt-3 buttons-mobile">
-            <Button variant="primary" className="nav-cta mb-2">Book Now</Button>
-            <Button variant="primary" className="nav-cta2">
-              <a href="tel:9500412121" className="phone-link">
-                <FontAwesomeIcon icon={faPhone} /> +91 9500412121
-              </a>
-            </Button>
-          </div>
         </Navbar.Collapse>
 
-        {/* Buttons for Larger Screens */}
-        <div className="d-none d-lg-flex align-items-center buttons-desktop">
-          <Button variant="primary" className="ms-2 nav-cta">Book Now</Button>
-          <Button variant="primary" className="ms-2 nav-cta2">
-            <a href="tel:9500412121" className="phone-link">
-              <FontAwesomeIcon icon={faPhone} /> +91 9500412121
+        {/* Right side - Book Now CTA */}
+        <div className="d-flex flex-lg-row flex-column gap-3">
+          <Button variant="primary" className="nav-cta">
+            Book Now
+          </Button>
+          <Button variant="primary" className="nav-cta2">
+            <a
+              href="tel:9500412121"
+              className="text-white text-decoration-none"
+            >
+              <FontAwesomeIcon icon={faPhone} className="me-2" />
+              +91 9500412121
             </a>
           </Button>
         </div>
       </Container>
+      {showMegaMenu && (
+        <div
+          className="mega-menu"
+          onMouseEnter={() => setShowMegaMenu(true)}
+          onMouseLeave={() => setShowMegaMenu(false)}
+        >
+          <div className="container">
+            <div className="row justify-content-center">
+              {activeMenu === "slimming" && (
+                <div className="col-md-10">
+                  <h3 className="text-center">Slimming Treatments</h3>
+                  <div className="row">
+                    {slimming.map((treatment) => (
+                      <div className="col-md-6" key={treatment}>
+                        <div
+                          className="mega-menu-item"
+                          onClick={() => handleNavigate(treatment)}
+                        >
+                          {treatment}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+              {activeMenu === "skin" && (
+                <div className="col-md-10">
+                  <h3 className="text-center">Skin & Hair Treatments</h3>
+                  <div className="row">
+                    {skin.map((treatment) => (
+                      <div className="col-md-6" key={treatment}>
+                        <div
+                          className="mega-menu-item"
+                          onClick={() => handleNavigate(treatment)}
+                        >
+                          {treatment}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </Navbar>
   );
 };
